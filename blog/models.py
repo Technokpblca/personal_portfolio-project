4e09e1c36c10f3bd5b1ea33d744f000351991f06
Abbat <misochka-piva@yandex.ru>
2022-07-06 16:22:04 +0300

    New commit

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,7 @@ class Blog(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='portfolio/images/', blank=True)
     date = models.DateField()
+
+
+    def __str__(self):
+        return self.title
